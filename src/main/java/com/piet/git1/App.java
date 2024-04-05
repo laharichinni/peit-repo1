17c536892100b116e8acd0dbb747b4632d2e2337
code <code@gmail.com>
2024-04-05 11:35:35 +0530

    go to code

---
+++ src/main/java/com/piet/git1/App.java
@@ -0,0 +1,21 @@
+package com.piet.git1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data)
+	{
+		System.out.println("data="+data);
+	}
+	public void eat() 
+	{
+		System.out.println("eating");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
